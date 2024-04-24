f85ec0039dcc4c3e7a854bf2dcc473e022010dc2
Ilyes Kadji <web.kin.dev@gmail.com>
2024-04-24 20:44:43 +0200

    projet.json / creation gallerie en js

---
+++ script.js
@@ -0,0 +1,76 @@
+// index.js
+
+// Charger les projets à partir du fichier JSON
+fetch('projets.json')
+  .then(response => response.json())
+  .then(data => {
+    const projets = data.projets;
+    const projetList = document.querySelector('.les-projets');
+
+    // Afficher chaque projet dans une liste
+    projets.forEach(projet => {
+      const item = document.createElement('li');
+      item.innerHTML = `
+        <a data-name="${projet.categorie}" href="${projet.lienSite}" title="Rendu du site">
+          <figure>
+            <img
+              src="${projet.image}"
+              alt="${projet.altText}"
+            />
+            <figcaption>
+              <a
+                href="${projet.lienRepo}"
+                title="Repo Github"
+              >
+                <h3>${projet.titre}</h3>
+                <p>${projet.description}</p>
+              </a>
+            </figcaption>
+          </figure>
+        </a>
+      `;
+      projetList.appendChild(item);
+    });
+  })
+  .catch(error => {
+    console.error('Erreur lors du chargement des projets : ', error);
+  });
+
+  const projetsFilter = document.querySelector('.filtres');
+
+  const filtres = document.querySelector('.filtres');
+  filtres.addEventListener('click', function(event) {
+    
+    const target = event.target;
+    if (target.tagName === 'A') {
+      const actives = document.querySelectorAll('.active');
+      actives.forEach(function(active) {
+        active.classList.remove('active');
+       
+      });
+      target.parentElement.classList.add('active');
+    }
+  });
+
+
+const filtresBtn = document.querySelectorAll('.filtres li');
+const lesProjets = document.querySelector('.les-projets');
+
+const name = item.dataset.name;
+
+filtresBtn.forEach(function(btn) {
+  btn.addEventListener('click', function() {
+    lesProjets.innerHTML = '';
+    const name = btn.dataset.name;
+    projets.forEach(function(projet) {
+      if (projet.categorie === name || name === 'Tous') {
+        const item = document.createElement('li');
+        item.innerHTML = `item${projet.categorie}`;
+        lesProjets.appendChild(item);
+      }
+    });
+  });
+});
+
+
+
